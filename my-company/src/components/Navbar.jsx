1cbb76e99934808f099d5bab83663a24040d290c
Esrael <belayesrael5@gmail.com>
2025-07-27 22:37:43 +0300

    3rd commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -10,4 +10,23 @@ function Navbar() {
     </nav>
   );
 }
-export default Navbar;
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        backgroundColor: '#222',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '10px',
+        marginBottom: '20px'
+      }}
+    >
+      <Link to="/" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
+}
