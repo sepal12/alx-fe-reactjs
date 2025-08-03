e5c6f706ce7ff6fd464edc4814e13537070baa12
Esrael <belayesrael5@gmail.com>
2025-08-03 22:48:36 +0300

    updating zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,54 +1,12 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-}));import create from 'zustand'
-
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-}));
-
-
-
-
-import React from 'react';
-import { useRecipeStore } from './recipeStore';
-
-const SearchBar = () => {
-  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-
-  return (
-    <input
-      type="text"
-      placeholder="Search recipes..."
-      onChange={(e) => setSearchTerm(e.target.value)}
-      style={{ marginBottom: '1rem', width: '100%', padding: '0.5rem' }}
-    />
-  );
-};
-import { create } from 'zustand';
-
-  create((set, get) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
   searchTerm: '',
   filteredRecipes: [],
   addRecipe: (newRecipe) =>
-    set((state) => {const updatedRecipes = [...state.recipes, newRecipe];
+    set((state) => {
+      const updatedRecipes = [...state.recipes, newRecipe];
       return {
         recipes: updatedRecipes,
         filteredRecipes: get().filterRecipes(updatedRecipes, state.searchTerm),
@@ -91,45 +49,3 @@ import { create } from 'zustand';
     );
   },
 }));
-
-import { Link } from 'react-router-dom';
-
-const RecipeList = () => {
-  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
-
-  return (
-    <div>
-      {filteredRecipes.length === 0 && <p>No recipes found.</p>}
-      {filteredRecipes.map(recipe => (
-        <div key={recipe.id}>
-          <Link to={`/recipe/${recipe.id}`}>
-            <h3>{recipe.title}</h3>
-          </Link>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-
-function App() {
-  return (
-    <Router>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
