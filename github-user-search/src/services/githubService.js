587d4873f280d45ccf588e74ee9b884dddf7b232
Esrael <belayesrael5@gmail.com>
2025-08-10 15:24:33 +0300

    commit on githab service.js file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -162,3 +162,37 @@ export const fetchUserData = async (username) =>
   };
   
   export default Search;
+
+    import axios from 'axios';
+  async (username) => {
+    const response = await axios.get(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+          ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+          : undefined,
+      },
+    });
+    return response.data;
+  };
+    let query = '';
+    if (username) query += `${username} in:login`;
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>=${minRepos}`;
+  
+    try {
+      const response = await axios.get('https://api.github.com/search/users', {
+        params: {
+          q: query.trim(),
+          per_page: 10,
+          page,
+        },
+        headers: {
+          Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+            ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+            : undefined,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
