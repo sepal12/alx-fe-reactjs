de4c1cc83c684e5b501f2ece473b35c1a30e33b3
Esrael <belayesrael5@gmail.com>
2025-08-10 22:51:41 +0300

    commit on adding effects

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User profile" 
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4 md:my-5 text-center">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 hover:text-blue-500 transition-colors duration-300 my-3 sm:my-4 md:my-5 text-center">
         John Doe
       </h1>
       <p className="text-sm sm:text-base text-gray-600 text-center">
